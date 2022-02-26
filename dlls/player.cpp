399edf324360c5503cab03080ce183025dcb04ae
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2022-02-26 05:12:17 +0400

    See if player with FL_SPECTATOR flag instead of EF_NODRAW for safe check

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -1633,7 +1633,7 @@ void CBasePlayer::PlayerUse ( void )
 	if ( ! ((pev->button | m_afButtonPressed | m_afButtonReleased) & IN_USE) )
 		return;
 
-	if ( (pev->button & IN_USE) && (pev->effects == EF_NODRAW) )
+	if ( (pev->button & IN_USE) && (pev->flags & FL_SPECTATOR) )
 		return;
 
 	// Hit Use on a train?
